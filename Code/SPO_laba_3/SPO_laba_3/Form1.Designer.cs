8ab41ef7d58dd1d3ba1326952ac46eb0eadc0cff
Syrym <www.sirim@mail.ru>
2016-05-24 16:35:21 +0600

    2-oi commit

--- Code/SPO_laba_3/SPO_laba_3/Form1.Designer.cs
+++ Code/SPO_laba_3/SPO_laba_3/Form1.Designer.cs
@@ -29,36 +29,57 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::SPO_laba_3.Properties.Resources.reb;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(164, 124);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(191, 183);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(693, 468);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(524, 461);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "SPO Laba 3";
+            this.TransparencyKey = System.Drawing.Color.White;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Code/SPO_laba_3/SPO_laba_3/Form1.cs
+++ Code/SPO_laba_3/SPO_laba_3/Form1.cs
@@ -16,5 +16,96 @@ namespace SPO_laba_3
         {
             InitializeComponent();
         }
+
+        public Form form1 = new Form();
+         
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            //if (e.Control && e.Alt && e.KeyCode == Keys.A)
+            //keysmod == Keys.Alt
+            Keys keysmod = Control.ModifierKeys;
+            if (e.Alt)
+            {
+                if (e.KeyCode == Keys.K)
+                {
+                    pictureBox1.Visible = true;
+                }
+            }
+            //-------------------------  Верх  ------------
+            if (e.KeyCode == Keys.Up)
+            {
+                int y = 10;
+                label1.Text = "Верх";
+                pictureBox1.Visible = true;  // Временно
+
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - y);
+                
+                if (pictureBox1.Location.Y < -170)
+                {
+                    int up_Y;
+                    
+                    form1.Size = new Size(form1.Size.Width, form1.Size.Height);
+                    up_Y = form1.Size.Height + 150;
+
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, up_Y);
+                }
+
+            }
+            //-------------------------  Вниз  ------------
+            if (e.KeyCode == Keys.Down)
+            {
+                int y = 10;
+                label1.Text = "Вниз";
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + y);
+
+                form1.Size = new Size(form1.Size.Width, form1.Size.Height);
+                if (pictureBox1.Location.Y > (form1.Size.Height+140))
+                {
+                    int dn_Y = -175;
+
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, dn_Y);
+                }
+            }
+            
+            // ------------------------  Влево  <<------------
+            if (e.KeyCode == Keys.Left)
+            {
+                int x = 10;
+                label1.Text = "Влево";
+                pictureBox1.Location = new Point(pictureBox1.Location.X - x, pictureBox1.Location.Y);
+
+                if (pictureBox1.Location.X < -170)
+                {
+                    int left_X;
+                    form1.Size = new Size(form1.Size.Width, form1.Size.Height);
+                    left_X = form1.Size.Width + 200;
+                    pictureBox1.Location = new Point(left_X, pictureBox1.Location.Y);
+                }
+
+            }
+
+            // ------------------------  Вправо  ------------>>
+            if (e.KeyCode == Keys.Right)
+            {
+                int x = 10;
+                label1.Text = "Вправо";
+                pictureBox1.Location = new Point(pictureBox1.Location.X + x, pictureBox1.Location.Y);
+
+                form1.Size = new Size(form1.Size.Width, form1.Size.Height);
+                if (pictureBox1.Location.X > (form1.Size.Width + 205))
+                {
+                    int left_X = -175;
+
+                    pictureBox1.Location = new Point(left_X, pictureBox1.Location.Y);
+                }
+
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
